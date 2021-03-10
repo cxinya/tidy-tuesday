87258a0681e6ce12f6ce1bf8f89a35639f3d4396
cxinya <30631351+cxinya@users.noreply.github.com>
2021-03-09 22:47:32 -0500

    Basic waffle

--- 2021_week-11_bechdel/2021_week-11_bechdel.R
+++ 2021_week-11_bechdel/2021_week-11_bechdel.R
@@ -1,4 +1,9 @@
 library(tidyverse)
+library(waffle)
+library(showtext)
+
+
+# Data --------------------------------------------------------------------
 
 raw_bechdel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-09/raw_bechdel.csv')
 
@@ -22,12 +27,28 @@ genre_bechdel <- movies %>%
   left_join(select(raw_bechdel, imdb_id, rating)) %>% 
   drop_na(genre, rating) %>% 
   group_by(genre) %>% 
-  count(rating) %>% 
-  mutate(per = n / sum(n))
+  count(rating)
 
 top10_genres <- genre_bechdel %>% 
   summarize(tot_films = sum(n)) %>% 
-  slice_max(tot_films, n = 10)
+  slice_max(tot_films, n = 10) %>% 
+  arrange(tot_films)
 
 genre_bechdel <- genre_bechdel %>% 
-  filter(genre %in% top10_genres$genre)
+  filter(genre %in% top10_genres$genre) %>% 
+  mutate(
+    rating = as.character(rating),
+    genre = factor(genre, levels = top10_genres$genre)
+    )
+
+# Viz ---------------------------------------------------------------------
+
+
+genre_bechdel %>% ggplot() +
+  facet_wrap(~genre, nrow = 1, strip.position = "bottom") +
+  geom_waffle(
+    aes(values = n, fill = rating),
+    n_rows = 10, size = .35, colour = "white", flip = T, show.legend = F) +
+  coord_equal()
+
+ggsave("test.png", width = 10, height = 5, units = "in")
