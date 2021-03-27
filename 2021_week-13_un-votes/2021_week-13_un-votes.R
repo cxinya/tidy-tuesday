41505b0e87c3f73e8224d8b5bd740e654fb72a7d
cxinya <30631351+cxinya@users.noreply.github.com>
2021-03-26 21:56:30 -0400

    stacked bar by yaer

--- 2021_week-13_un-votes/2021_week-13_un-votes.R
+++ 2021_week-13_un-votes/2021_week-13_un-votes.R
@@ -1,22 +1,17 @@
 library(tidyverse)
-library(ggstream)
+library(patchwork)
+library(showtext)
 
 unvotes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/unvotes.csv')
 roll_calls <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/roll_calls.csv')
 issues <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-23/issues.csv')
 
-# Brainstorm
-# % role calls about issues over time -- stream on top and # votes in panel below?
-# Close votes
-# US vs the world
 
+font_add_google("Poppins", "pop")
+showtext_auto()
 
 pal <- c("#5b92e5", "#B5BFC8", "#E6DC5C")
 
-
-
-
-# US vs the world ---------------------------------------------------------
 
 # Votes per resolution
 
@@ -34,6 +29,7 @@ vote_res <- unvotes %>%
   replace(is.na(.), 0) %>%
   left_join(select(roll_calls, rcid, importantvote)) %>%
   filter(!is.na(importantvote)) %>%
+  # Vote results - simple majority for regular resolutions, 2/3 for important issues to pass
   mutate(
     result = case_when(
       importantvote == 0 & (no == 50 | yes == 50)       ~ "No decision",
@@ -45,7 +41,9 @@ vote_res <- unvotes %>%
       importantvote == 0 & yes > 50     ~ "Pass",
       importantvote == 1 & yes > 200/3  ~ "Pass"))
 
-# Add US votes and see how they match
+
+# Add US votes and see whether they match
+
 us_match <- vote_res %>%
   left_join(filter(unvotes, country_code == "US")) %>%
   mutate(
@@ -59,10 +57,38 @@ us_match <- vote_res %>%
   left_join(select(issues, -short_name)) %>%
   left_join(select(roll_calls, rcid, date)) %>%
   mutate(year = lubridate::year(date)) %>%
-  filter(!is.na(country_code) & result != "No decision")
+  filter(!is.na(country_code) & result != "No decision") %>%
+  arrange(date) %>%
+  mutate(order = row_number()) # Orig RCIDs not in chronological order
 
 
-# Match/mismatch by year --------------------------------------------------
+
+# Mass/mismatch by topic
+
+us_match_topic_plot <- us_match %>%
+  filter(!is.na(issue)) %>%
+  ggplot() +
+  facet_wrap(vars(issue), ncol = 1) +
+  geom_segment(
+    aes(x = order, xend = order, y = 0, yend = 1, color = us_vs_world)
+  ) +
+  geom_text(
+    aes(x = 0, y = 0, label = toupper(issue)),
+    vjust = 0, hjust = 0, fontface = "bold", size = 10, color = "#0C274F", family = "pop") +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0)) +
+  scale_color_manual(values = pal) +
+  theme_void() +
+  theme(
+    strip.text = element_blank(),
+    legend.position = "none",
+    panel.spacing = unit(0, "lines"),
+    plot.background = element_rect(fill = "white", color = "NA")
+  )
+
+ggsave("test.png")
+
+# Match/mismatch by year
 
 us_match_year <- us_match %>%
   group_by(year) %>%
@@ -70,38 +96,19 @@ us_match_year <- us_match %>%
   group_by(year) %>%
   mutate(per = n / sum(n) * 100)
 
-us_match_year %>%
+us_match_year_plot <- us_match_year %>%
   ggplot(aes(x = year, y = per, fill = us_vs_world, color = us_vs_world)) +
-  geom_bar(position = "stack", stat = "identity") +
-  # geom_stream(bw = .8, alpha = .9, size = 1) +
-  # geom_stream_label(
-  #   aes(label = us_vs_world),
-  #   lineheight = .5, fontface = "bold") +
-  scale_fill_manual(values = pal3) +
-  scale_color_manual(values = pal3) +
+  geom_bar(position = "stack", stat = "identity", width = 1) +
+  scale_fill_manual(values = pal) +
+  scale_color_manual(values = pal) +
   scale_x_continuous(breaks = seq(1945, 2020, 5), labels = seq(1945, 2020, 5)) +
-  theme_minimal()
-
-
-# Mass/mismatch by topic --------------------------------------------------
-
-us_match_topic <- us_match %>%
-  filter(!is.na(issue) & rcid < 6000) %>%
-  ggplot() +
-  facet_wrap(vars(issue), ncol = 1) +
-  geom_segment(
-    aes(x = rcid, xend = rcid, y = 0, yend = 1, color = us_vs_world),
-    size = 1
-  ) +
-  geom_text(
-    aes(x = 0, y = .05, label = toupper(issue)),
-    vjust = 0, hjust = 0, fontface = "bold", size = 10, color = "#0C274F") +
-  scale_x_continuous(expand = c(0,0)) +
-  scale_y_continuous(expand = c(0,0)) +
-  scale_color_manual(values = rev(pal3)) +
   theme_void() +
   theme(
     strip.text = element_blank(),
     legend.position = "none",
-    panel.spacing = unit(0, "lines")
+    panel.spacing = unit(0, "lines"),
+    plot.background = element_rect(fill = "white", color = "NA")
   )
+
+
+# Combine plots
